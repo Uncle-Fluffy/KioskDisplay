794b764570c43110bdd7c2c536af8ee3fbf6de64
Terry Carter <tcarter@asrcfederal.com>
2025-05-23 14:37:11 -0500

    local_dimming.sh trying out magick -modulate rather than -brightness-contrast

--- bashscripts/local_dimming.sh
+++ bashscripts/local_dimming.sh
@@ -3,12 +3,22 @@ mkdir -p evening
 mkdir -p night
 
 # Dimming values. Higher numbers are brighter
-evening_brightness="20"
-night_brightness="60"
+# evening_brightness="20"
+# night_brightness="60"
+# Modulate values. Higher numbers are brighter
+evening_brightness="80"
+night_brightness="40"
+
+# Calculate saturation as half of brightness
+evening_saturation=$((evening_brightness / 2))
+night_saturation=$((night_brightness / 2))
 
 for imagefile in *-2.jpg; do
+  echo "Updating $imagefile..."
   eveningfile="evening/${imagefile}"
   nightfile="night/${imagefile}"
-  magick "$imagefile" -brightness-contrast -"${evening_brightness}"x0 "$eveningfile"
-  magick "$imagefile" -brightness-contrast -"${night_brightness}"x0 "$nightfile"
+#  magick "$imagefile" -brightness-contrast -"${evening_brightness}"x0 "$eveningfile"
+#  magick "$imagefile" -brightness-contrast -"${night_brightness}"x0 "$nightfile"
+  magick "$imagefile" -modulate "${evening_brightness},${evening_saturation}",100 "$eveningfile" # Brightness, Saturation, Hue
+  magick "$imagefile" -modulate "${night_brightness},${night_saturation}",100 "$nightfile"
 done
